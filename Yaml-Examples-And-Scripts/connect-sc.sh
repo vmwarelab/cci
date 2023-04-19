62a70116536f0f9f07a2cfe03ba8803db484633d
Maher AlAsfar <malasfar@vmware.com>
2023-04-19 08:07:20 -0400

    Updates

--- Yaml-Examples-And-Scripts/connect-sc.sh
+++ Yaml-Examples-And-Scripts/connect-sc.sh
@@ -1,5 +1,5 @@
 export SC_IP=Your_Supervisor_IP
 export KUBECTL_VSPHERE_PASSWORD=Your_vSphere_Password
-kubectl vsphere login --server=https://$SC_IP --vsphere-username administrator@vsphere.local --insecure-skip-tls-verify
+kubectl vsphere login --server=https://$SC_IP --vsphere-username Your_vSphere_Username --insecure-skip-tls-verify
 
 
--- Yaml-Examples-And-Scripts/connect-tkc-1.sh
+++
@@ -1,4 +0,0 @@
-export SC_IP=Your_Supervisor_IP
-export NAMESPACE=Your_Namespace
-export KUBECTL_VSPHERE_PASSWORD=Your_vSphere_Password
-kubectl vsphere login --server=https://$SC_IP --tanzu-kubernetes-cluster-name tkc-1 --tanzu-kubernetes-cluster-namespace $NAMESPACE --vsphere-username administrator@vsphere.local --insecure-skip-tls-verify
