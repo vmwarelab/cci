fbc9e3f9436674c079dda69745e42e8d14fd3406
Maher AlAsfar <malasfar@vmware.com>
2023-09-22 12:23:19 -0400

    updating ccs to cci

--- Yaml-Examples-And-Scripts/connect-cci-user.sh
+++ Yaml-Examples-And-Scripts/connect-cci-user.sh
@@ -1,3 +1,3 @@
 export CCI_API_TOKEN=Your_User_Token
 export SERVER=Your_API_Server
-kubectl ccs login -t $CCI_API_TOKEN --server $SERVER --insecure-skip-tls-verify
+kubectl cci login -t $CCI_API_TOKEN --server $SERVER --insecure-skip-tls-verify
