07ccfd179c8c562f5a14787f43cda72f301f95bd
Maher AlAsfar <malasfar@vmware.com>
2023-10-02 08:40:22 -0400

    renaming ccs to cci in the scripts

--- Admin-Script/README.md
+++ Admin-Script/README.md
@@ -7,19 +7,19 @@ You need to follow the instructions to
 * Deploy a vSphere testbed (or use an existing one)
 * Deploy the Cloud Proxy
 * Create the Cloud Account in vRA
-* Make sure you have kubectl and the ccs plugin installed
+* Make sure you have kubectl and the cci plugin installed
 
 You can run the script `create.sh` after updating the variables in the file and logging in.
 
 Edit these values in the script.
 
-`PROJECT`: Name of the project in CCS
+`PROJECT`: Name of the project in cci
 
 `REGION`: Unique region name for the project
 
 `SV_NAMESPACE_CLASS`: Name of the SV namespace class that will be created.
 
-`CCS_USER`: User that needs to be granted permission to operate on the namespace. 
+`cci_USER`: User that needs to be granted permission to operate on the namespace. 
 This should be your LDAP user id.
 
 `STORAGE_CLASS`: The name of the storage class that will be used. It needs to be available in the SV.
@@ -28,11 +28,11 @@ This should be your LDAP user id.
 
 Login
 ```
-kubectl ccs login -s  api_server_fqdn  -t  ORG_API_TOKEN  --skip-set-context --insecure-skip-tls-verify
+kubectl cci login -s  api_server_fqdn  -t  ORG_API_TOKEN  --skip-set-context --insecure-skip-tls-verify
 ```
-Make sure you are in the ccs context.
+Make sure you are in the cci context.
 ```
-kubectl config use-context ccs
+kubectl config use-context cci
 ```
 Run the script
 ```
@@ -43,13 +43,13 @@ You need to then edit the supervisor resource to assign it to the region created
 
 Find your SV cluster:
 ```
-kubectl -n ccs-config get supervisors
+kubectl -n cci-config get supervisors
 ```
 
 Then update it to add the label and region:
 
 ```
-kubectl -n ccs-config edit supervisor ${YOUR_SV_CLUSTER}
+kubectl -n cci-config edit supervisor ${YOUR_SV_CLUSTER}
 ```
 
 Add a label representing the supervisor in a key value pair formatn ( for Example environment: prod ):
@@ -64,4 +64,4 @@ Add this to the spec, matching the ${REGION} created above
     - ${matching the ${REGION} above}
 ```
 
-You should be able to login to the CCS environment and see the project and be able to create namespaces.
+You should be able to login to the cci environment and see the project and be able to create namespaces.
--- Admin-Script/create.sh
+++ Admin-Script/create.sh
@@ -22,40 +22,40 @@ export STORAGE_CLASS=Your_storge_class_name
 export ENVIRONMENT=Your_Env_Name
 
 # Make sure you are logged in before executing this script
-# kubectl ccs login -s  api_server_fqdn  -t  ORG_API_TOKEN  --skip-set-context --insecure-skip-tls-verify
+# kubectl cci login -s  api_server_fqdn  -t  ORG_API_TOKEN  --skip-set-context --insecure-skip-tls-verify
 
 # Created by an admin to group users and set access to infrastructure resources. 
-envsubst < project.yaml | kubectl --context ccs create -f -
+envsubst < project.yaml | kubectl --context cci create -f -
 
-# Created by an admin to assign a role per user. CCS will validate one resource per user.
-envsubst < projectrolebinding.yaml | kubectl --context ccs create -f -
+# Created by an admin to assign a role per user. cci will validate one resource per user.
+envsubst < projectrolebinding.yaml | kubectl --context cci create -f -
 
 # Created by an admin to group one or many supervisors in a region.
-envsubst < region.yaml | kubectl --context ccs create -f -
+envsubst < region.yaml | kubectl --context cci create -f -
 
 # The admin will be able to update the Supervisor adding the region and with labels that will be used for namespace placement
 # TBD Write some sed script to automate this
 echo "Be sure to Patch Supervisor - Region / Labels per documentation"
 echo "---------------------------------------------------------------"
-echo "STEP 1 : Get list of supervisors :  kubectl -n ccs-config get supervisors"
-echo "STEP 2 : Edit the superisor to add Region and Labels :  kubectl -n ccs-config edit supervisor <Supervisor Name>"
+echo "STEP 1 : Get list of supervisors :  kubectl -n cci-config get supervisors"
+echo "STEP 2 : Edit the superisor to add Region and Labels :  kubectl -n cci-config edit supervisor <Supervisor Name>"
 echo "---------------------------------------------------------------"
 echo "Use this Label: "${ENVIRONMEN}
 echo "Use this region: "${REGION}
 
 # Created by an admin to define namespace templates with optional parameters. The resource should not contain any namespace settings.
-envsubst < supervisornamespaceclass.yaml | kubectl create --context ccs -f -
+envsubst < supervisornamespaceclass.yaml | kubectl create --context cci -f -
 
 # Created by an admin to define namespace class configuration with namespace settings. Parameter values can be used in namespace settings. 
-envsubst < supervisornamespaceclassconfig.yaml | kubectl create --context ccs -f -
+envsubst < supervisornamespaceclassconfig.yaml | kubectl create --context cci -f -
 
 # Created by an admin to allow project accessing supervisors in a region. The resource should not contain any Supervisors placement settings.
-envsubst < regionbinding.yaml | kubectl --context ccs create -f -
+envsubst < regionbinding.yaml | kubectl --context cci create -f -
 
 # Create or edit a Supervisor Namespace Class Config with additional expressions to match the Supervisor tags.
-envsubst < regionbindingconfig.yaml | kubectl --context ccs create -f -
+envsubst < regionbindingconfig.yaml | kubectl --context cci create -f -
 
 # Created by an admin to allow creating a Supervisor Namespace using the Supervisor Namespace Class in a project.
-envsubst < supervisornamespaceclassbinding.yaml | kubectl create --context ccs -f -
+envsubst < supervisornamespaceclassbinding.yaml | kubectl create --context cci -f -
 
 
